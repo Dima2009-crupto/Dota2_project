36cadd3a3392c6c9d7c11f4c061b439ac47d1728
Dima2009-crupto <dimaparoldima1234@gmail.com>
2025-07-22 13:18:49 +0200

    first commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    sqlalchemy_uri: str = "mysql+aiomysql://localhost:5432/DOTA2_PROJECT"
+    secret_key: str = "super secret key"
+    exp_time_minutes: int = 30
+
+
+settings = Settings()
+
